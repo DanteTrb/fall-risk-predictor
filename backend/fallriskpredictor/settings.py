9108e2f5729c35ae5b6d579f4a89ecfd6f23a489
Dante Trabassi <dantetrb@gmail.com>
2025-06-12 19:48:56 +0200

    Fix: add Render domain to ALLOWED_HOSTS

--- backend/fallriskpredictor/settings.py
+++ backend/fallriskpredictor/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = 'django-insecure-REPLACE_ME_WITH_A_SECURE_KEY'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fall-risk-predictor.onrender.com']
 
 # Application definition
 INSTALLED_APPS = [
